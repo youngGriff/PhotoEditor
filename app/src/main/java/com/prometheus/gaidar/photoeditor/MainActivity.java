8ba0b36b23889709b9abfe675e4c5d5e3a5a95e7
Andriy Gaidar <andriy.gaidar99@gmail.com>
2017-10-08 15:30:46 +0300

    Now works better.

--- app/src/main/java/com/prometheus/gaidar/photoeditor/MainActivity.java
+++ app/src/main/java/com/prometheus/gaidar/photoeditor/MainActivity.java
@@ -4,7 +4,6 @@ import android.app.ProgressDialog;
 import android.graphics.Bitmap;
 import android.graphics.PointF;
 import android.graphics.Rect;
-import android.graphics.RectF;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -12,83 +11,38 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.widget.Toast;
 
 import com.theartofdev.edmodo.cropper.CropImageView;
 
 import jp.co.cyberagent.android.gpuimage.GPUImage;
 import jp.co.cyberagent.android.gpuimage.GPUImageBulgeDistortionFilter;
 
-public class MainActivity extends AppCompatActivity implements ImageWithCircleView.BitmapCropListener {
-    CropImageView mainImageView;
+public class MainActivity extends AppCompatActivity implements CropImageView.OnSetCropOverlayMovedListener, CropImageView.OnCropImageCompleteListener {
+    private CropImageView ivBulgeView;
     private GPUImageBulgeDistortionFilter bulgeFilter;
     private GPUImage gpuImage;
-    private ImageView imageView;
-    SeekBar sbRadius;
-    SeekBar sbScale;
+    private ImageView ivPreview;
+    private SeekBar sbScale;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sample_image_with_circle_view);
-        mainImageView = findViewById(R.id.image);
-        imageView = findViewById(R.id.iv_cropImage);
-        sbRadius = findViewById(R.id.sb_radius);
+        setContentView(R.layout.activity_main);
+        ivBulgeView = findViewById(R.id.image);
+        ivPreview = findViewById(R.id.iv_cropImage);
+
         sbScale = findViewById(R.id.sb_scale);
 
-        Bitmap bitmap = ((BitmapDrawable) getResources().getDrawable(R.drawable.main)).getBitmap();
+        Bitmap bitmap = ((BitmapDrawable) getResources().getDrawable(R.drawable.test)).getBitmap();
 
-        mainImageView.setImageBitmap(bitmap);
+        ivBulgeView.setImageBitmap(bitmap);
         bulgeFilter = new GPUImageBulgeDistortionFilter();
         gpuImage = new GPUImage(this);
         gpuImage.setImage(bitmap);
 
-
-        sbScale.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-
-
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-        sbRadius.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-
-
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-
     }
 
-    @Override
-    public void showImage(Bitmap bitmap) {
-        imageView.setImageBitmap(bitmap);
-    }
-
-    @Override
-    public void hideImage() {
-        imageView.setImageBitmap(null);
-    }
 
     public void applyEffect(View view) {
         final ProgressDialog progressDialog = new ProgressDialog(this);
@@ -96,14 +50,18 @@ public class MainActivity extends AppCompatActivity implements ImageWithCircleVi
         progressDialog.setTitle("Please wait");
         progressDialog.show();
 
-        Rect wholeImageRect = mainImageView.getWholeImageRect();
-        Rect areaRect = mainImageView.getCropRect();
+        Rect wholeImageRect = ivBulgeView.getWholeImageRect();
+        Rect areaRect = ivBulgeView.getCropRect();
+
         float i = sbScale.getProgress();
         float scale = ((i - 10.F)) / 10;
-        Log.i("scale", scale + "");
+        float radius = (areaRect.width() / 2) / wholeImageRect.width();
+        PointF center = new PointF(areaRect.exactCenterX() / wholeImageRect.width(),
+                areaRect.exactCenterY() / wholeImageRect.height());
+        Log.i("parameters", "scale is" + scale + "\n radius is " + radius
+                + "\n center: " + center.toString());
         bulgeFilter.setScale(scale);
-        bulgeFilter.setCenter((new PointF(areaRect.exactCenterX() / wholeImageRect.width(),
-                areaRect.exactCenterY() / wholeImageRect.height())));
+        bulgeFilter.setCenter(center);
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -113,11 +71,25 @@ public class MainActivity extends AppCompatActivity implements ImageWithCircleVi
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        mainImageView.setImageBitmap(bitmap);
+                        ivBulgeView.setImageBitmap(bitmap);
                         progressDialog.dismiss();
                     }
                 });
             }
         }).start();
+    }
+
+    @Override
+    public void onCropImageComplete(CropImageView view, CropImageView.CropResult result) {
+        ivPreview.setImageBitmap(result.getBitmap());
+    }
+
+    public void saveTo(View view) {
+        Toast.makeText(this, "Feature is not yet realized", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onCropOverlayMoved(Rect rect) {
+        ivBulgeView.getCroppedImageAsync();
     }
 }
